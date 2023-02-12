cdb096832ea0e968239b8ede8b4b02362eca42de
crazyboi <dilipkumarsahoo2003@gmail.com>
2023-02-12 21:38:56 +0530

    created the pages n changed FootnNav used jquery taking an exam gap resume 25thfeb2023

---
+++ custom/every-pg-custom.js
@@ -0,0 +1,33 @@
+/*==================================================
+        [ Search Button ]
+        ==================================================*/
+jQuery("#pq-seacrh-btn").on("click", function () {
+  jQuery(".pq-search-form").slideToggle();
+  jQuery(".pq-search-form").toggleClass("pq-form-show");
+  if (jQuery(".pq-search-form").hasClass("pq-form-show")) {
+    jQuery(this).html('<i class="fas fa-times"></i>');
+  } else {
+    jQuery(this).html('<i class="fas fa-search"></i>');
+  }
+});
+/*==================================================
+          [ Back To Top ]
+          ==================================================*/
+jQuery("#back-to-top").fadeOut();
+jQuery(window).on("scroll", function () {
+  if (jQuery(this).scrollTop() > 250) {
+    jQuery("#back-to-top").fadeIn(1400);
+  } else {
+    jQuery("#back-to-top").fadeOut(400);
+  }
+});
+jQuery("#top").on("click", function () {
+  jQuery("top").tooltip("hide");
+  jQuery("body,html").animate(
+    {
+      scrollTop: 0,
+    },
+    800
+  );
+  return false;
+});
